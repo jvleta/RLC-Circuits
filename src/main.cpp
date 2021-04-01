23f5b40875e4bff97488e43a1434e6ea9b84d5b3
Jimmy Leta <JLeta@minitab.com>
2021-03-31 20:42:33 -0400

    removing call to vtk plotting function (for now)

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,8 @@
 #include <vector>
 
 #include "menus.h"
-#include "plotter.h"
 
 int main() {
-  // MainMenu();
-  int numPoints = 69;
-  float inc = 7.5 / (numPoints - 1);
-  std::vector<double> time(numPoints, 0.0);
-  std::vector<double> response(numPoints, 0.0);
-  for (int i = 0; i < numPoints; ++i) {
-    time[i] = i * inc;
-    response[i] = std::cos(i * inc);
-  }
-
-  makeplots(time, response);
-  
+  MainMenu();
   return 0;
 }
